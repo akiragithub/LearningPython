f68c46ed0543da801b39513512ce85a1bef968c4
akiragithub <akiratech1@gmail.com>
2018-11-28 14:19:27 +0800

    Python for trading scripts_4

--- PythonProjects/application_2.py
+++ PythonProjects/application_2.py
@@ -10,6 +10,7 @@ This script displays a random dataset using a noisy linear process
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import pymc3 as pm
 
 def simulate_linear_data(N,beta_0,beta_1,eps_sigma_sq):
     
@@ -42,6 +43,28 @@ def simulate_linear_data(N,beta_0,beta_1,eps_sigma_sq):
     df["y"] = beta_0 + beta_1*df["x"] + np.random.RandomState(42).normal(eps_mean, eps_sigma_sq, N)
     return df
 
+def glm_mcmc_inference(df, iterations=5000):
+    """
+    Calculates the Markov Chain Monte Carlo trace of a generalised linear model bayesian linear regression model on a supplied data
+    df: DataFrame containing the supplied data
+    iterations: number of iterations to carry out MCMC for
+    """
+    
+    # Use pymc3 to construct a model context
+    basic_model = pm.Model()
+    with basic_model :
+        # Create a generalised linear model (glm) according to df
+        # Use the normal family distribution for the likelihood
+        pm.glm.GLM.from_formula('y ~ x', df, family=pm.glm.families.Normal())
+        # Use Maximum A Posteori (MAP) as initial value for MCMC
+        start = pm.find_MAP()
+        # Setting the steps with the No-U-Turn Sampler algorithm
+        step = pm.NUTS()
+        # Calculate the trace
+        trace = pm.sample(iterations, step, start, random_seed=42, progressbar=True, cores=1)
+    return trace
+        
+
 if __name__=="__main__" :
     #Setting or parameters
     beta_0 = 1.0
@@ -51,6 +74,21 @@ if __name__=="__main__" :
     eps_sigma_sq = 0.5
     #Simulate the linear noisy data
     simulate_linear_data(N,beta_0,beta_1,eps_sigma_sq)
-    #plot everything
+    #plot the data and a frequentist linear regression fit 
     sns.lmplot(x="x", y="y", data=df, size=10)
     plt.xlim(0.0, 1.0)
+    
+    trace = glm_mcmc_inference(df, iterations=5000)
+    pm.traceplot(trace[500:])
+    plt.show()
+    
+    # Plot a sample of posterior regression lines
+    sns.lmplot(x="x", y="y", data=df, size=10, fit_reg=False)
+    plt.xlim(0.0,1.0)
+    plt.ylim(0.0,4.0)
+    pm.plot_posterior_predictive_glm
+    
+    
+    
+    
+    
